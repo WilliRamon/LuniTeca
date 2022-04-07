f732daa24aa498186a06c508765c71897e577e9c
WilliRamon <acdc.ramon@hotmail.com>
2022-04-07 02:14:22 -0300

    Construindo camada EmprestimoController

---
+++ luniteca/src/main/java/com/luniteca/luniteca/controller/EmprestimoController.java
@@ -0,0 +1,51 @@
+package com.luniteca.luniteca.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luniteca.luniteca.model.Emprestimo;
+import com.luniteca.luniteca.repository.EmprestimoRepository;
+
+@RestController
+@RequestMapping("/emprestimo")
+@CrossOrigin(origins="*", allowedHeaders = "*")
+public class EmprestimoController {
+	
+	@Autowired
+	private EmprestimoRepository emprestimoRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Emprestimo>> getAll(){
+		return ResponseEntity.ok(emprestimoRepository.findAll());
+	}
+	
+	@GetMapping("/dataEmprestimo/{dataEmprestimo}")
+	public ResponseEntity<List<Emprestimo>> getByDataEmprestimo(@PathVariable Date dataEmprestimo) {
+		return ResponseEntity.ok(emprestimoRepository.findAllByDataEmprestimoContaining(dataEmprestimo));
+	}
+	
+	@GetMapping("/dataEntre/{inicio}/{fim}")
+	public ResponseEntity<List<Emprestimo>> getByDataEntre(@PathVariable Date inicio, @PathVariable Date fim) {
+		return ResponseEntity.ok(emprestimoRepository.findByDataEmprestimoBetween(inicio, fim));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Emprestimo> postEmprestimo(@Valid @RequestBody Emprestimo emprestimo){
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(emprestimoRepository.save(emprestimo));
+	}
+
+}
--- luniteca/src/main/java/com/luniteca/luniteca/repository/EmprestimoRepository.java
+++ luniteca/src/main/java/com/luniteca/luniteca/repository/EmprestimoRepository.java
@@ -1,5 +1,8 @@
 package com.luniteca.luniteca.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.luniteca.luniteca.model.Emprestimo;
 
 @Repository
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{
+	
+	public List<Emprestimo> findAllByDataEmprestimoContaining (Date dataEmprestimo);
+	
+	public List<Emprestimo> findByDataEmprestimoBetween(Date inicio, Date fim);
 
 }
